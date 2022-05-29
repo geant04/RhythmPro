8af5cc5e89dbfb27c78a5dd6ed7c9d308254bff0
Anthony <75352073+planthony04@users.noreply.github.com>
2022-05-28 20:26:06 -0400

    Add accuracy, highscores

--- rhythm/Assets/Scripts/GameManager.cs
+++ rhythm/Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -28,12 +31,14 @@ public class GameManager : MonoBehaviour
     public float scorePerNote = 100;
     public int streak;
 
-    private int hits = 0;
-    private int misses = 0;
+    private int hits;
+    public static int[] scoreData = {0,0,0,0}; //fine, nice, excellent, miss
+    private int accSum = 0;
 
     public static GameManager instance;
     public BeatSpawner bSpawner;
     public bool startPlaying;
+    public bool playedSong;
     public MenuController pauseMenu;
     public KeyCode keyToPress;
 
@@ -43,17 +48,23 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public Text streakText;
     public Text timeText;
+    public Text accuracyText;
 
     public static MidiFile midiFile;
 
     public GameObject resultScreen;
-    public Text pHitValue, mHitValue, percHit, rank, fScore;
+    public Text[] resultText;
+    public RawImage imageRank;
+    public Texture[] rankTextures = new Texture[6];
 
-    public int[] hitScore = {50,100,300};
+    public int[] hitScore = {50,200,300,0};    
     public string[] hitTexts = {"Fine", "Nice", "Excellent", "Miss"};
 
     public List<GameObject> hitObjects;
     List<GameObject> objs = new List<GameObject>();
+
+    public string mainMenuScene;
+
 
     // Start is called before the first frame update
     void Start()
@@ -71,14 +82,15 @@ public class GameManager : MonoBehaviour
     {
         if(!startPlaying)
         {
-            if(Input.GetKeyDown(keyToPress)){
+            if(Input.GetKeyDown(keyToPress) && !playedSong){
+                playedSong = true;
                 resultScreen.SetActive(false);
                 Time.timeScale = 1f;
 
                 score = 0f;
                 streak = 0;
                 hits = 0;
-                misses = 0;
+                Array.Clear(scoreData, 0, scoreData.Length);
 
                 scoreText.text = "Score: " + score;
                 streakText.text = "" + streak;
@@ -137,25 +149,38 @@ public class GameManager : MonoBehaviour
         if(timeHit < 0.05f) scoretype = 2;
 
         score += hitScore[scoretype] + Mathf.Ceil(streak * 5.5f);
+        scoreData[scoretype] += 1;
+        accSum += hitScore[scoretype];
+
         streak += 1;
         hits += 1;
 
         scoreText.text = "Score: " + score;
         streakText.text = ""+ streak;
+        adjustAccuracy();
 
         spawnHitMessage(scoretype);
-
     }
+
     public void NoteMiss(){
-        misses += 1;
+        scoreData[3] += 1;
 
         missSound.Play();
 
+        hits += 1;
         streak = 0;
         streakText.text = "" + streak;
+        adjustAccuracy();
 
         spawnHitMessage(3);
     }
+
+    public void adjustAccuracy()
+    {
+        float tempAcc = (float) accSum / (hits * 3f);
+        accuracyText.text = "Accuracy: " + tempAcc.ToString("0.00") + "%";
+    }
+
     public void spawnHitMessage(int scoretype)
     {
         var msg = Instantiate(hitObjects[scoretype], transform);
@@ -172,23 +197,83 @@ public class GameManager : MonoBehaviour
     {
         resultScreen.SetActive(true);
 
-        pHitValue.text = "" + hits;
-        mHitValue.text = "" + misses;
+        int sumScore = 0;
+        int numElem = 0;
+        string finalScore = score + "-";
 
-        percHit.text = Mathf.Ceil((float)hits/(float)(hits+misses) * 100f) + "%";
+
+        for(int i=0; i<scoreData.Length; i++)
+        {
+            resultText[i].text = "" + scoreData[i];
+            sumScore += (scoreData[i] * hitScore[i]);
+            numElem += scoreData[i];
+
+            // begin saving data
+
+            finalScore += scoreData[i].ToString() + ";";
+        }
+
+        float acc = (float)sumScore / (numElem * 3f); // (sum/(elem * 300) * 100)
+
+        resultText[4].text = acc.ToString("0.00") + "%";
+        resultText[5].text = "" + score;
+
+        float[] R = {1f, 1f, 0f, 0f, 0.4902f, 0.7176f};
+        float[] G = {0.8178f, 0.8178f, 0.7169f, 0.6370f, 0f, 0f};
+        float[] B = {0f, 0f, 0.0044f, 0.7176f, 0.7176f, 0f};
+
+        int indx = returnRank(acc);
+        imageRank.texture = rankTextures[indx];
+        imageRank.color = new Color(R[indx], G[indx], B[indx], 1f);
+
+        // time to save the data...
         
-        rank.text = returnRank(Mathf.Ceil((float)hits/(float)(hits+misses) * 100f));
-        fScore.text = "" + score;
+        if(PlayerPrefs.HasKey(GameManager.musicSelectedName))
+        {            
+            string getScore = PlayerPrefs.GetString(GameManager.musicSelectedName);
+            
+            string[] splitWord = getScore.Split("-");
+            int tempScore = int.Parse(splitWord[0]);
+
+            Debug.Log("Comparing other score...\nPrevious highscore: " + tempScore);
+
+            if(score > tempScore)
+            {
+                Debug.Log("New highscore for " + GameManager.musicSelectedName + ": " + finalScore);
+                PlayerPrefs.SetString(GameManager.musicSelectedName, finalScore);
+            }
+        }else //no data
+        {
+            PlayerPrefs.SetString(GameManager.musicSelectedName, finalScore);
+            Debug.Log("Logged score for " + GameManager.musicSelectedName + ": " + finalScore);
+        }
+        
     }
-    public static string returnRank(float percentage)
+
+    public static int returnRank(float percentage) // {SS, S, A, B, C, D}
+    {   
+        if(scoreData[3] == 0)
+        {
+            if(percentage == 100) return 0;
+            if(percentage > 95) return 1;
+        }
+        if(percentage > 90) return 2;
+        if(percentage > 80) return 3;
+        if(percentage > 70) return 4;
+        return 5;
+    }
+    /////////
+    public void replay()
     {
-        if(percentage == 100) return "SS";
-        if(percentage > 95) return "S";
-        if(percentage > 90) return "A";
-        if(percentage > 80) return "B";
-        if(percentage > 70) return "C";
-        return "D";
+        playedSong = false;
+        resultScreen.SetActive(false);
+        streakText.text = "Press \"space\" to play";
     }
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+    /////////
     public static bool playing()
     {
         return instance.theMusic.isPlaying;
--- rhythm/Assets/Scripts/SongSelect.cs
+++ rhythm/Assets/Scripts/SongSelect.cs
@@ -12,18 +12,82 @@ public class SongSelect : MonoBehaviour
 
     public AudioSource[] songSources;
     public AudioSource previewMusic;
-    public string[] songNames = {"Yellow - Coldplay", "Helicopter - Bloc Party", "Through the Fire and Flames - Dragonforce"};
+    public string[] songNames = {"Yellow - Coldplay", "Helicopter - Bloc Party", "Flower Dance - DJ Okawari"};
+
+    public RawImage sample;
+    public Texture[] textures = new Texture[3];
+    
+    public RawImage imageRank;
+    public Texture[] rankTextures = new Texture[6];
+
+    public Text accuracy;
+    public Text displayScore;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameManager.musicSelectedIndex = index;
+        GameManager.musicSelectedName = songSources[index].name;
         
+        sample.texture = textures[index];
+
+        songTitle.text = songNames[index];
+
+        previewMusic = songSources[index];
+        previewMusic.Play();
+
+        showSongInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void showSongInfo()
+    {
+        if(PlayerPrefs.HasKey(GameManager.musicSelectedName))
+        {
+            imageRank.enabled = true;
+
+            string getScore = PlayerPrefs.GetString(GameManager.musicSelectedName);
+            
+            Debug.Log("Obtained: " + getScore);
+
+            string[] splitWord = getScore.Split("-");
+            displayScore.text = splitWord[0];
+
+
+            string noteData = splitWord[1];
+            string[] dataString = noteData.Split(";");
+            
+            int sumScore = 0;
+            int numElem = 0;
+            int[] scoreValues = {50,200,300,0};
+
+            Debug.Log("Test");
+
+            for(int i=0; i<4; i++){
+                sumScore += ( int.Parse(dataString[i])  * scoreValues[i] );
+                numElem += int.Parse(dataString[i]);
+            }
+
+            float acc = (float)sumScore / (numElem * 3f); // (sum/(elem * 300) * 100)
+            accuracy.text = acc.ToString("0.00") + "%";
+            
+            float[] R = {1f, 1f, 0f, 0f, 0.4902f, 0.7176f};
+            float[] G = {0.8178f, 0.8178f, 0.7169f, 0.6370f, 0f, 0f};
+            float[] B = {0f, 0f, 0.0044f, 0.7176f, 0.7176f, 0f};
+
+            int indx = GameManager.returnRank(acc);
+            imageRank.texture = rankTextures[indx];
+            imageRank.color = new Color(R[indx], G[indx], B[indx], 1f);
+        }else // no song detected
+        {
+            imageRank.enabled = false;
+            accuracy.text = "";
+            displayScore.text = "";
+        }
     }
     public void nextSong(){
         previewMusic.Stop();
@@ -33,13 +97,18 @@ public class SongSelect : MonoBehaviour
         GameManager.musicSelectedIndex = index;
         GameManager.musicSelectedName = songSources[index].name;
 
+        sample.texture = textures[index];
+
 
         songTitle.text = songNames[index];
 
         previewMusic = songSources[index];
         previewMusic.Play();
+
+        showSongInfo();
     }
     public void playSong(){
+        
         SceneManager.LoadScene(firstLevel);
     }
 }
